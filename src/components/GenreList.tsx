c1784cbae3584e3287ce18cab7ec9bd32cd8c20e
Rawle Becerril <rawlesxm@gmail.com>
2023-09-05 20:35:04 -0400

    bold genre selection

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,10 +3,11 @@ import useGenres, {Genre} from "../hooks/useGenres"
 import getCroppedImageURL from "../services/image-url"
 
 interface Props {
-    onSelectGenre: (genre: Genre) => void
+    onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre} : Props) => {
+const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
     const {data, isLoading, error} = useGenres()
 
     if (error) return null
@@ -22,7 +23,7 @@ const GenreList = ({onSelectGenre} : Props) => {
                 borderRadius={8}
                 src={getCroppedImageURL(genre.image_background)}
                 />
-                <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
+                <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
                     {genre.name}
                 </Button>
             </HStack>
